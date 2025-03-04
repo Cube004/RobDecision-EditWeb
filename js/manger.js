8a49cd1d1ce1ae96399d97c3c2988aa9f9e9bd77
CUBE <13724238+cube004@user.noreply.gitee.com>
2025-03-04 11:32:21 +0800

    Test-0.0.15

--- js/manger.js
+++ js/manger.js
@@ -3,7 +3,6 @@ let lastscale = 1;
 let scale = 1;
 
 
-
 class ObjectManager{
     constructor(){
         this.NodecontainerId = "Nodecontainer";
@@ -37,24 +36,18 @@ class ObjectManager{
     }
 
     deleteNode(id) {
-        // 删除对应的按钮
         const button = document.querySelector(`button[data-id="${id}"]`);
         if (button) {
             button.remove();
         }
-        // 过滤掉目标节点
         this.NodeList = this.NodeList.filter(node => node.id !== id);
-        // 重新排序剩余节点的ID，并更新DOM元素和按钮属性
         this.NodeList.forEach((node, index) => {
-            const oldId = node.id; // 保存旧ID以定位按钮
+            const oldId = node.id;
             const newId = `Node_${index}`;
-            // 更新节点ID
             node.id = newId;
-            // 更新节点对应的DOM元素ID
             if (node.element) {
                 node.element.id = newId;
             }
-            // 找到旧ID对应的按钮，更新其data-id属性
             const oldButton = document.querySelector(`button[data-id="${oldId}"]`);
             if (oldButton) {
                 oldButton.setAttribute('data-id', newId);
@@ -64,15 +57,38 @@ class ObjectManager{
 
     addLine(points){
         let canMerge = false;
+        let edge = null;
         this.EdgeList.forEach(Edge => {
             canMerge = Edge.CheckCanMerge(points)
+            if (canMerge) edge = Edge;
         })
         if (!canMerge) {
             let polyline = new Line(points, `temp_${Math.random()}`, null);
-            this.EdgeList.push(new Edge(this.EdgecontainerId ,`Edge_${this.EdgeList.length}`, polyline));
+            edge = new Edge(this.EdgecontainerId ,`Edge_${this.EdgeList.length}`, polyline)
+            this.EdgeList.push(edge);
         }
+        this.CheckConnection(edge)
     }
-    
+
+    CheckConnection(Edge){
+        
+        const isPointInRect =(node, point) =>{
+            let rectX = node.left - gridSize;
+            let rectY = node.top - gridSize;
+            let rectWidth = node.width + 2 * gridSize;
+            let rectHeight = node.height + 2 * gridSize;
+            let x = point.x;
+            let y = point.y;
+            const xRight = rectX + rectWidth;
+            const yBottom = rectY + rectHeight;
+            return x >= rectX && x <= xRight && y >= rectY && y <= yBottom;
+        }
+        this.NodeList.forEach(node => {
+            if (isPointInRect(node, Edge.Endpoints.In)) Edge.nodeIn = node;
+            if (isPointInRect(node, Edge.Endpoints.Out)) Edge.nodeOut = node;
+        })
+        Edge.UpdateView();
+    }
 }
 
 class Node{
@@ -291,24 +307,111 @@ class Line {
 class Edge{
     constructor(containerId, id, BasePolyline){
         this.nodeIn = null;
-        this.nodeTo = null;
+        this.nodeOut = null;
         this.id = id
         this.polylineList = [];
         this.roundsize = gridSize;
         this.container = document.getElementById(containerId)
+        this.Endpoints = {
+            In: { x: 0, y: 0 },
+            Out: { x: 0, y: 0 }
+        }
+        this.Endpoints = {
+            In: { x: BasePolyline.points[0].x, y: BasePolyline.points[0].y },
+            Out: { x: BasePolyline.points[1].x, y: BasePolyline.points[1].y }
+        }
 
         this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         this.svg.setAttribute("data-id", this.id);
         this.linewidth = 3;
         this.container.appendChild(this.svg);
 
-        BasePolyline.id = `Edge_${this.id}_${this.polylineList.length}`
-        BasePolyline.element.setAttribute("data-id", BasePolyline.id);
+
         BasePolyline.BindEdge(this);
-        this.polylineList.push(BasePolyline);
-        this.svg.appendChild(BasePolyline.element);
+        this.AddnewLine(BasePolyline);
 
         this.UpdateSize();
+    }
+
+    Arrow(x, y, padding){
+        let Min = { x: x, y: y };
+        let width = 24;
+        let height = 24;
+        const arrow = document.querySelector(`path[data-id="${this.id}_Arrow"]`);
+        if(arrow) {
+            arrow.remove();
+        }
+        
+        let EndLine = null;
+        let Otherpoint = null;
+        this.polylineList.forEach(polyline => {
+            polyline.points.forEach(point => {
+                if(point.x == this.Endpoints.Out.x && point.y == this.Endpoints.Out.y){
+                    EndLine = polyline;
+                    return;
+                }
+            })
+        })
+        if(EndLine === null){
+            console.log("Error: EndLine is null");
+            console.log(EndLine);
+            return;
+        }
+        if(EndLine.points[0].x == this.Endpoints.Out.x && EndLine.points[0].y == this.Endpoints.Out.y){
+            Otherpoint = EndLine.points[1];
+        }else{
+            Otherpoint = EndLine.points[0];
+        }
+
+        if(Otherpoint === null || EndLine === null){
+            console.log("Error: Otherpoint or EndLine is null");
+            console.log(Otherpoint, EndLine);
+            return;
+        }
+        let angle = Math.atan2(Otherpoint.y - this.Endpoints.Out.y, Otherpoint.x - this.Endpoints.Out.x) * 180 / Math.PI;
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", "m3.414 7.086-.707.707a1 1 0 0 0 0 1.414l7.778 7.778a2 2 0 0 0 2.829 0l7.778-7.778a1 1 0 0 0 0-1.414l-.707-.707a1 1 0 0 0-1.415 0l-7.07 7.07-7.072-7.07a1 1 0 0 0-1.414 0Z");
+        path.setAttribute("fill", "currentColor");
+        path.setAttribute("data-id", `${this.id}_Arrow`);
+        path.setAttribute("transform", `translate(${(-Min.x + padding + this.Endpoints.Out.x - width / 2) * scale},${(-Min.y + padding + this.Endpoints.Out.y - height / 2) * scale}),rotate(${angle + 90} ${12 * scale} ${12 * scale}),scale(${scale})`);
+        this.svg.appendChild(path);
+    }
+
+    EndpointsShow(x, y, padding){
+        let Min = { x: x, y: y };
+        const nodeInCircle = document.querySelector(`path[data-id="${this.id}_In"]`);
+        if(nodeInCircle) {
+            nodeInCircle.remove();
+        }
+        if(this.nodeIn === null){
+            const circle = this.createCircularPath(this.Endpoints.In.x, this.Endpoints.In.y, 5);
+            circle.setAttribute('data-id', `${this.id}_In`);
+            circle.setAttribute("transform", `translate(${(-Min.x + padding) * scale},${(-Min.y + padding) * scale})`);
+            circle.setAttribute("fill", "red");
+            this.svg.appendChild(circle);
+        }
+        const nodeToCircle = document.querySelector(`path[data-id="${this.id}_Out"]`);
+        if(nodeToCircle) {
+            nodeToCircle.remove();
+        }
+        if(this.nodeOut === null){
+            const circle = this.createCircularPath(this.Endpoints.Out.x, this.Endpoints.Out.y, 5);
+            circle.setAttribute('data-id', `${this.id}_Out`);
+            circle.setAttribute("transform", `translate(${(-Min.x + padding) * scale},${(-Min.y + padding) * scale})`);
+            circle.setAttribute("fill", "green");
+            this.svg.appendChild(circle);
+        }
+    }
+
+    createCircularPath(cx, cy, radius) {
+        cx = cx * scale;
+        cy = cy * scale;
+        radius = radius * scale;
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        const d = `M ${cx-radius},${cy} a ${radius},${radius} 0 1,0 ${radius*2},0 a ${radius},${radius} 0 1,0 ${-radius*2},0`;
+        path.setAttribute("d", d);
+        return path;
     }
 
     UpdateSize() {
@@ -324,9 +427,9 @@ class Edge{
             });
         });
     
-        const padding = this.linewidth + 5;
-        const paddingX = padding / scale;
-        const paddingY = padding / scale;
+        const padding = this.linewidth + 8;
+        const paddingX = padding * scale;
+        const paddingY = padding * scale;
     
         const adjustedMin = {
             x: Min.x - paddingX,
@@ -344,8 +447,10 @@ class Edge{
         this.svg.style.height = `${(adjustedMax.y - adjustedMin.y) * scale}px`;
     
         this.polylineList.forEach(line => {
-            line.element.setAttribute("transform", `translate(${(-Min.x + paddingX) * scale},${(-Min.y + paddingX) * scale})`);
+            line.element.setAttribute("transform", `translate(${(-Min.x + paddingX) * scale},${(-Min.y + paddingY) * scale})`);
         });
+        this.EndpointsShow(Min.x, Min.y, padding * scale);
+        this.Arrow(Min.x, Min.y, padding * scale);
     }
     
     UpdateView(){
@@ -353,6 +458,15 @@ class Edge{
             line.Redraw();
             if(line.round){
                 this.svg.appendChild(line.element);
+            }
+            if (this.nodeIn != null && this.nodeOut != null) {
+                line.element.dasharray = "none"
+                line.element.setAttribute("stroke-dasharray", "none");
+                console.log("实线");
+                
+            }else{
+                line.element.dasharray = `${this.linewidth * scale},${this.linewidth * scale}`;
+                line.element.setAttribute("stroke-dasharray", `${this.linewidth * scale},${this.linewidth * scale}`);
             }
         })
         this.UpdateSize();
@@ -376,6 +490,17 @@ class Edge{
             ]
             return candidates.find(pair => EqualPoint(pair.a, pair.b))?.a || null
         }
+        const areLinesPerpendicularOrParallel = (points1, points2) => {
+            const [p1, p2] = points1;
+            const [p3, p4] = points2;
+            const dx1 = p2.x - p1.x;
+            const dy1 = p2.y - p1.y;
+            const dx2 = p4.x - p3.x;
+            const dy2 = p4.y - p3.y;
+            if (dx1 * dy2 === dx2 * dy1) return false;
+            if (dx1 * dx2 + dy1 * dy2 === 0) return true;
+        }
+
         let canMerge = false;
         let round = false;
         this.polylineList.forEach(polyline => {
@@ -386,8 +511,15 @@ class Edge{
 
             if(junction != null){
                 canMerge = true;
-                round = (points1.at(1).x - points1.at(0).x > gridSize / 10 * scale) 
-                    === (points2.at(1).y - points2.at(0).y > gridSize / 10 * scale);
+
+                if(EqualPoint(this.Endpoints.Out, junction) === null){
+                    this.Endpoints.In = EqualPoint(points[0], junction) === null ? points[0] : points[1];
+                }else{
+                    this.Endpoints.Out = EqualPoint(points[0], junction) === null ? points[0] : points[1];
+                }
+                console.log(this.Endpoints);
+                
+                round = areLinesPerpendicularOrParallel(points1, points2);
                 if(round){
                     // 计算交点处的圆角
                     let roundpoints = this.GetRoundPoints(points1, points2, junction);
